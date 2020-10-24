4defe8e530e435ab5c3e710ebed0da021960c852
naokin <naokieru8r@gmail.com>
2020-10-24 19:41:10 +0900

    Flask add template

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-
+ 
 
 @app.route('/')
-def index():
-    return 'hello, thank you '
+def main():
+    return render_template('main.html')
+
+#@app.route('/<post_name>') #動的ルーティング
+#def show_post(post_name):
+#    print(type(post_name))
+#    return 'Post {}'.format(post_name)
 
 if __name__=='__main__':
     app.run()
